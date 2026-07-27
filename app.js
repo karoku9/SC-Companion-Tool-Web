3cc78dbbf54aaca9d111c28d01a67944535ac87d
karoku9 <59685586+karoku9@users.noreply.github.com>
2026-07-27 20:29:22 +0200

    Load stable manual cargo grid view

--- app.js
+++ app.js
@@ -144,7 +144,7 @@
     .then(() => import('./operations-flow-v028.js'))
     .then(() => import('./operations-readable-short-desktop-v0291.js'))
     .then(() => import('./operations-cargo-guidance-v0292.js'))
-    .then(() => import('./cargo-manual-grid-view-v030.js'))
+    .then(() => import('./cargo-manual-grid-view-v0301.js'))
     .then(() => import('./ship-selector-sync.js'))
     .then(() => import('./missions-operations-bridge.js'))
     .then(() => window.dispatchEvent(new Event('sc:dynamic-pages-ready')))
